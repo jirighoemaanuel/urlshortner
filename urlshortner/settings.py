f06569bc6263f842298c9000681966a0f4b92b97
jirighoemaanuel <emmaweyinmi@gmail.com>
2021-02-14 11:14:41 +0100

    commit

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 # django_heroku.settings(locals())
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 prod_db = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update((prod_db,))
+# DATABASES['default'].update((prod_db,))
